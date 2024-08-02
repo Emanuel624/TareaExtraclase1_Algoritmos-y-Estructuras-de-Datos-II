09b0ff90f587fe15d50e721a0313a5bdad44979b
Emanuel624 <emanuel.chavarria.hernandez@gmail.com>
2024-08-01 22:40:23 -0600

    Se agregan versiones beta de los algortimos de ordenamiento necesarios para el programa sorter.cpp (en desarrollo)

---
+++ sorter.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+//QuickSort
+int partition(int arr[], int start, int end) {
+    int pivot = arr[start];
+    int count = 0;
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i] <= pivot)
+            count++;
+    }
+    int pivotIndex = start + count;
+    swap(arr[pivotIndex], arr[start]);
+    int i = start, j = end;
+    while (i < pivotIndex && j > pivotIndex) {
+        while (arr[i] <= pivot) {
+            i++;
+        }
+        while (arr[j] > pivot) {
+            j--;
+        }
+        if (i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
+    }
+    return pivotIndex;
+}
+
+void quickSort(int arr[], int start, int end) {
+    if (start >= end)
+        return;
+    int p = partition(arr, start, end);
+    quickSort(arr, start, p - 1);
+    quickSort(arr, p + 1, end);
+}
+
+int main() {
+    int arr[] = { 9, 3, 4, 2, 1, 8 };
+    int n = 6;
+    quickSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
+
+//////////////////////////////////////////////
+//InsertionSort
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void imprimirArreglo(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    imprimirArreglo(arr, n);
+
+    return 0;
+}
+
+////////////////////////////////////////////////
+//BubbleSOrt
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Intercambiar arr[j] y arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void imprimirArreglo(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    cout<<"Arreglo ordenado: \n";
+    imprimirArreglo(arr, n);
+    return 0;
+}
