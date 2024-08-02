b1db0a10385d7ae992dffec8bd46d57b300bbd27
Emanuel624 <emanuel.chavarria.hernandez@gmail.com>
2024-08-01 22:09:32 -0600

    Se agrega las verifaciones finales del programa generator.cpp asi como cambios de logica para mejorar la generacion del archivo binario

--- generator.cpp
+++ generator.cpp
@@ -22,41 +22,42 @@ int main(int argc, char* argv[])
         }
     }
 
+    int iterations;
+    if (size == "SMALL"){
+        iterations = 128000000;
+    } else if (size == "MEDIUM"){
+        iterations = 250000000;
+    } else if (size == "LARGE"){
+        iterations = 500000000;
+    } else {
+        cout << "Error: SIZE debe ser SMALL, MEDIUM o LARGE.\n";
+        return 1; // Terminar el programa con un código de error
+    }
+
     fstream fout; //Inicializa el objeto fstreamg+
     fout.open(outputFilePath, ios::out | ios::binary); //Se abre/genera un archivo de tipo binario
 
     if (fout)
     {
-        if (size == "SMALL"){
-            // 512MB es aproximadamente 512000000 bytes
-            // Un int tiene 4 bytes, por lo que necesitamos generar aproximadamente 128000000 números
-            for(int i = 0; i < 128000000; i++)
-            {
-                int x = rand(); // Generar un número aleatorio
-                fout.write(reinterpret_cast<char*>(&x), sizeof(int));
+        const int bufferSize = 1000000; // Tamaño del buffer
+        int* buffer = new int[bufferSize]; // Crear el buffer
+
+        for(int i = 0; i < iterations; i++)
+        {
+            buffer[i % bufferSize] = rand(); // Generar un número aleatorio y guardarlo en el buffer
+
+            // Cuando el buffer esté lleno, escribirlo en el archivo y vaciarlo
+            if(i % bufferSize == bufferSize - 1){
+                fout.write(reinterpret_cast<char*>(buffer), bufferSize * sizeof(int));
             }
         }
 
-        if (size == "MEDIUM"){
-            // 1GB es aproximadamente 1000000000 bytes
-            // Un int tiene 4 bytes, por lo que necesitamos generar aproximadamente 250000000 números enteros
-            for(int i = 0; i < 250000000; i++)
-            {
-                int x = rand(); // Generar un número aleatorio
-                fout.write(reinterpret_cast<char*>(&x), sizeof(int));
-            }
+        // Escribir los números restantes en el buffer
+        if(iterations % bufferSize != 0){
+            fout.write(reinterpret_cast<char*>(buffer), (iterations % bufferSize) * sizeof(int));
         }
 
-        if (size == "LARGE"){
-            //2GB es aproximadamente 2000000000 bytes
-            // Un int tiene 4 bytes, por lo que se deben generar 500000000 números enteros
-            for (int i = 0; i < 500000000; i++)
-            {
-                int x = rand(); // Generar un número aleatorio
-                fout.write(reinterpret_cast<char*>(&x), sizeof(int));
-            }
-        }
-
+        delete[] buffer; // Liberar la memoria del buffer
         fout.close();
     }else{
         cout << "Error abriendo el archivo!\n";
